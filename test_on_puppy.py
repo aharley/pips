35847f3007325041b4d731404cd4a8e4c2b267b7
aharley <harley.adam@gmail.com>
2022-07-24 17:07:18 -0700

    cleaned up puppy demo

--- test_on_puppy.py
+++ test_on_puppy.py
@@ -3,7 +3,7 @@ import argparse
 import numpy as np
 import timeit
 import matplotlib
-import tensorflow as tf
+# import tensorflow as tf
 # import scipy.misc
 import io
 import os
@@ -27,7 +27,7 @@ import imageio
 # from nets.st_graph_raft import StGraphRaft
 # from nets.mraft import Mraft
 # from nets.praft import Praft
-from nets.mpraft import Mpraft
+# from nets.mpraft import Mpraft
 from nets.singlepoint import Singlepoint
 # from perceiver_graph import PerceiverGraph
 # from relative_mlp import RelativeMlp
@@ -62,15 +62,15 @@ def fetch_optimizer(lr, wdecay, epsilon, num_steps, params):
 # import nets.segpointnet
 
 import utils.py
-import utils.box
+# import utils.box
 import utils.misc
 import utils.improc
-import utils.vox
-import utils.grouping
-from tqdm import tqdm
+# import utils.vox
+# import utils.grouping
+# from tqdm import tqdm
 import random
 import glob
-import color2d
+# import color2d
 
 # import detectron2
 # from detectron2 import model_zoo
@@ -85,8 +85,8 @@ from utils.basic import print_, print_stats
 
 # import relation_model
 
-import datasets
-import cater_pointtraj_dataset
+# import datasets
+# import cater_pointtraj_dataset
 
 import torch
 import torch.nn as nn
@@ -103,197 +103,67 @@ patch_size = 8
 random.seed(125)
 np.random.seed(125)
 
-def sequence_loss(flow_preds, flow_gt, vis, valid, gamma=0.8):
-    """ Loss function defined over sequence of flow predictions """
-    B, S, N, D = flow_gt.shape
-    assert(D==2)
-    B, S, N = vis.shape
-    B, N = valid.shape
-    # print('flow_preds[0]', flow_preds[0].shape)
-    # print('flow_gt', flow_gt.shape)
-    # print('valid', valid.shape)
-    
-    n_predictions = len(flow_preds)    
-    flow_loss = 0.0
-    # print('n_predictions', n_predictions)
-
-    for i in range(n_predictions):
-        i_weight = gamma**(n_predictions - i - 1)
-        fp = flow_preds[i]#[:,:,0:1]
-        T = fp.shape[1]
-        i_loss = (fp - flow_gt[:,:T]).abs() # B, T, N, 2
-        i_loss = torch.mean(i_loss, dim=[1,3]) # B, N
-        # print('i_loss', i_loss.shape)
-        # print('valid', valid.shape)
-        # flow_loss += i_weight * i_loss.mean()
-        # flow_loss += i_weight * (valid[:,:,None,None] * i_loss).mean()
-        # flow_loss += i_weight * (valid * i_loss).mean()
-        # flow_loss += i_weight * (valid * i_loss).mean()
-        flow_loss += i_weight * utils.basic.reduce_masked_mean(i_loss, valid)
-
-    epe = torch.sum((flow_preds[-1] - flow_gt)**2, dim=-1).sqrt() # B, S, N
-    # epe = epe.view(-1)
-    # print('epe', epe.shape)
-    # print('vis', vis.shape)
-
-    # epe_vis = utils.basic.reduce_masked_mean(epe, valid.unsqueeze(1)*vis[:,1:])
-    # epe_inv = utils.basic.reduce_masked_mean(epe, valid.unsqueeze(1)*(1.0-vis[:,1:]))
-    epe_vis = utils.basic.reduce_masked_mean(epe, valid.unsqueeze(1)*vis)
-    epe_inv = utils.basic.reduce_masked_mean(epe, valid.unsqueeze(1)*(1.0-vis))
-
-    # epe_inv2inv = utils.basic.reduce_masked_mean(epe, valid.unsqueeze(1) * (1.0 - (vis[:,1:] + vis[:,:-1]).clamp(0,1)))
-    epe_inv2inv = epe.mean() * 0
-    
-    metrics = {
-        'epe': epe.mean().item(),
-        'epe_vis': epe_vis.item(),
-        'epe_inv': epe_inv.item(),
-        'epe_inv2inv': epe_inv2inv.item(),
-        '1px': (epe < 1).float().mean().item(),
-        '3px': (epe < 3).float().mean().item(),
-        '5px': (epe < 5).float().mean().item(),
-        '10px': (epe < 10).float().mean().item(),
-        '30px': (epe < 30).float().mean().item(),
-    }
-
-    return flow_loss, metrics
 
 def run_model(model, rgbs, N2, sw):
-    total_loss = torch.tensor(0.0, requires_grad=True).to(device)
-    metrics = {
-        'epe': 0,
-        'epe_vis': 0,
-        'epe_inv': 0,
-        'epe_inv2inv': 0,
-    }
-
-    # flow = d['flow'].cuda().permute(0, 3, 1, 2)
     rgbs = rgbs.cuda().float() # B, S, C, H, W
 
     B, S, C, H, W = rgbs.shape
     rgbs_ = rgbs.reshape(B*S, C, H, W)
-    H_, W_ = 320, 512
-    sy = H_/H
-    sx = W_/W
-    rgbs_ = F.interpolate(rgbs_, (320, 512), mode='bilinear')
+    H_, W_ = 360, 640
+    rgbs_ = F.interpolate(rgbs_, (H_, W_), mode='bilinear')
     H, W = H_, W_
     rgbs = rgbs_.reshape(B, S, C, H, W)
-    
-    # target_trajs = torch.zeros((B, S, N2, 2), dtype=torch.float32, device='cuda')
-    # target_vis = torch.zeros((B, S, N2), dtype=torch.float32, device='cuda')
-    # target_valid = torch.zeros((B, N2), dtype=torch.float32, device='cuda')
-    target_trajs = torch.zeros((B, S, N2, 2), dtype=torch.float32, device='cuda')
-    target_vis = torch.zeros((B, S, N2), dtype=torch.float32, device='cuda')
-    target_valid = torch.zeros((B, N2), dtype=torch.float32, device='cuda')
-    # for b in range(B):
-    #     y0 = np.random.randint(0, H, size=[N2])
-    #     x0 = np.random.randint(0, W, size=[N2])
-    #     print('y0', y0, y0.shape)
-    #     print('x0', x0, x0.shape)
-    #     target_trajs[b,:,:,0] = torch.from_numpy(x0).cuda().reshape(1, N2)
-    #     target_trajs[b,:,:,1] = torch.from_numpy(y0).cuda().reshape(1, N2)
-    #     target_vis[b,0,:] = 1
-    #     target_valid[b,:] = 1
-    
 
+    # pick N2 points to track; we'll use a uniform grid
     N_ = np.sqrt(N2).round().astype(np.int32)
     grid_y, grid_x = utils.basic.meshgrid2d(B, N_, N_, stack=False, norm=False, device='cuda')
-    # grid_y, grid_x = utils.basic.meshgrid2d(B, N2, N2, stack=False, norm=True, device='cuda')
     grid_y = 8 + grid_y.reshape(B, -1)/float(N_-1) * (H-16)
     grid_x = 8 + grid_x.reshape(B, -1)/float(N_-1) * (W-16)
     xy = torch.stack([grid_x, grid_y], dim=-1) # B, N2*N2, 2
-    print('xy', xy.shape)
-    print_stats('grid_x', grid_x)
-    print_stats('grid_y', grid_y)
-    target_trajs = xy.unsqueeze(1).repeat(1, S, 1, 1)
-
-    target_trajs = target_trajs.permute(0,2,1,3).reshape(B*N2, S, 1, 2)
-    target_vis = target_vis.permute(0,2,1).reshape(B*N2, S, 1)
-    target_valid = target_valid.reshape(B*N2, 1)
-    
-    B_bak = B
-    N2_bak = N2
-    
-    B = B*N2
-    N2 = 1
-
     _, S, C, H, W = rgbs.shape
 
-    if sw is not None and sw.save_this:
-        sw.summ_rgbs('inputs_0/rgbs', utils.improc.preprocess_color(rgbs[0:1]).unbind(1))
-
-        # sw.summ_traj2ds_on_rgbs('inputs_0/target_trajs_on_rgbs', target_trajs, utils.improc.preprocess_color(rgbs))
-        # sw.summ_traj2ds_on_rgbs('inputs_0/target_trajs_on_black', target_trajs, torch.ones_like(rgbs)*-0.5)
-        
-        # sw.summ_traj2ds_on_rgbs('inputs_0/trajs_on_rgbs', trajs[0:1], utils.improc.preprocess_color(rgbs[0:1]))
-        # sw.summ_traj2ds_on_rgbs('inputs_0/trajs_on_black', trajs[0:1], torch.ones_like(rgbs[0:1])*-0.5)
-
-        # sw.summ_traj2ds_on_rgb('inputs_0/single_trajs_on_rgb', trajs[0:1], torch.mean(utils.improc.preprocess_color(rgbs[0:1]), dim=1), cmap='winter')
-        # sw.summ_traj2ds_on_rgb('inputs_0/single_trajs_on_black', trajs[0:1], torch.ones_like(rgbs[0:1][:,0])*-0.5, cmap='winter')
-
-    # return total_loss, metrics
-
-    # print('trajs', trajs.shape)
-    # preds, preds2, cps, vis_e = model(target_trajs[:,0], rgbs, coords_init=None, iters=args.iters, sw=sw) # list of B, N, 2
-    # outs = model(target_trajs[:,0], rgbs, coords_init=None, iters=8, sw=sw) # list of B, N, 2
-    outs = model(target_trajs[:,0], rgbs, coords_init=None, iters=8) # list of B, N, 2
-    preds = outs[0]
-    preds2 = outs[1]
-    # print('preds[0]', preds[0].shape)
-
-    main_preds = [pred[:,:,0:1] for pred in preds]
-    # loss, metrics = sequence_loss(preds, targets, vis, valid, args.gamma)
-    loss, metrics = sequence_loss(main_preds, target_trajs, target_vis, target_valid, 0.8)
-    total_loss += loss
-
-    if total_loss > 200:
-        print('high loss; preparing special log')
-        sw.save_this = True
-
+    preds, preds_anim, vis_e, stats = model(xy, rgbs, iters=6)
     trajs_e = preds[-1]
+    print_stats('trajs_e', trajs_e)
 
-
-    print('rgbs', rgbs.shape)
     print(B, S, H, W)
-    pad = 100
-    rgbs = F.pad(rgbs.reshape(B_bak*S, 3, H, W), (pad, pad, pad, pad), 'constant', 0).reshape(B_bak, S, 3, H+pad*2, W+pad*2)
+    pad = 50
+    rgbs = F.pad(rgbs.reshape(B*S, 3, H, W), (pad, pad, pad, pad), 'constant', 0).reshape(B, S, 3, H+pad*2, W+pad*2)
     trajs_e = trajs_e + pad
-    target_trajs = target_trajs + pad
-
     
-    # print('trajs_e', trajs_e.shape)
-    trajs_e = trajs_e.reshape(B_bak, N2_bak, S, 2).permute(0, 2, 1, 3)
-    # print('trajs_e', trajs_e.shape)
-    target_trajs = target_trajs.reshape(B_bak, N2_bak, S, 2).permute(0, 2, 1, 3)
-
-
     if sw is not None and sw.save_this:
 
-        # sw.summ_traj2ds_on_rgb('outputs/gt_trajs_on_rgb' % n, trajs, torch.mean(utils.improc.preprocess_color(rgbs), dim=1), cmap='winter')
-        # sw.summ_traj2ds_on_rgb('outputs/gt_trajs_on_black' % n, trajs, torch.ones_like(rgbs[:,0])*-0.5, cmap='winter')
+        # sw.summ_rgbs('inputs_0/rgbs', utils.improc.preprocess_color(rgbs[0:1]).unbind(1))
+        # sw.summ_traj2ds_on_rgbs('outputs/trajs_on_rgbs', trajs_e[0:1], utils.improc.preprocess_color(rgbs[0:1]), cmap='spring', linewidth=2)
+        # sw.summ_traj2ds_on_rgbs('outputs/trajs_on_black', trajs_e[0:1], torch.ones_like(rgbs[0:1])*-0.5, cmap='spring', linewidth=2)
+        # sw.summ_traj2ds_on_rgbs2('outputs/trajs_on_rgbs2', trajs_e[0:1], vis_e[0:1], utils.improc.preprocess_color(rgbs[0:1]), cmap='spring')
 
-        sw.summ_traj2ds_on_rgbs('outputs/trajs_on_rgbs', trajs_e[0:1], utils.improc.preprocess_color(rgbs[0:1]), cmap='coolwarm')
-        sw.summ_traj2ds_on_rgbs('outputs/trajs_on_black', trajs_e[0:1], torch.ones_like(rgbs[0:1])*-0.5, cmap='coolwarm')
+        linewidth = 2
+        o1 = sw.summ_rgbs('inputs_0/rgbs', utils.improc.preprocess_color(rgbs[0:1]).unbind(1), only_return=True)
+        o2 = sw.summ_traj2ds_on_rgbs('outputs/trajs_on_rgbs', trajs_e[0:1], utils.improc.preprocess_color(rgbs[0:1]), cmap='spring', linewidth=linewidth, only_return=True)
+        o3 = sw.summ_traj2ds_on_rgbs('outputs/trajs_on_black', trajs_e[0:1], torch.ones_like(rgbs[0:1])*-0.5, cmap='spring', linewidth=linewidth, only_return=True)
+        print('o1', o1.shape)
+        wide = torch.cat([o1, o2, o3], dim=-1)
+        print('wide', wide.shape)
+        sw.summ_rgbs('outputs/wide', wide.unbind(1))
+        
+        # sw.summ_traj2ds_on_rgbs2('outputs/trajs_on_rgbs2', trajs_e[0:1], vis_e[0:1], utils.improc.preprocess_color(rgbs[0:1]), cmap='spring')
+        # gt_rgb = utils.improc.preprocess_color(sw.summ_traj2ds_on_rgb('inputs_0_all/single_trajs_on_rgb', target_trajs[0:1], torch.mean(utils.improc.preprocess_color(rgbs[0:1]), dim=1), cmap='winter', frame_id=metrics['epe'], only_return=True))
+        # gt_black = utils.improc.preprocess_color(sw.summ_traj2ds_on_rgb('inputs_0_all/single_trajs_on_rgb', target_trajs[0:1], torch.ones_like(rgbs[0:1,0])*-0.5, cmap='winter', frame_id=metrics['epe'], only_return=True))
+        # # gt_rgb = torch.mean(utils.improc.preprocess_color(rgbs), dim=1)
+        # # gt_black = torch.ones_like(rgbs[:,0])*-0.5
 
-        gt_rgb = utils.improc.preprocess_color(sw.summ_traj2ds_on_rgb('inputs_0_all/single_trajs_on_rgb', target_trajs[0:1], torch.mean(utils.improc.preprocess_color(rgbs[0:1]), dim=1), cmap='winter', frame_id=metrics['epe'], only_return=True))
-        gt_black = utils.improc.preprocess_color(sw.summ_traj2ds_on_rgb('inputs_0_all/single_trajs_on_rgb', target_trajs[0:1], torch.ones_like(rgbs[0:1,0])*-0.5, cmap='winter', frame_id=metrics['epe'], only_return=True))
-        # gt_rgb = torch.mean(utils.improc.preprocess_color(rgbs), dim=1)
-        # gt_black = torch.ones_like(rgbs[:,0])*-0.5
-
-        # animate_traj2ds_on_rgbs
-        sw.summ_traj2ds_on_rgb('outputs/single_trajs_on_gt_rgb', trajs_e[0:1], gt_rgb[0:1], cmap='coolwarm')
-        sw.summ_traj2ds_on_rgb('outputs/single_trajs_on_gt_black', trajs_e[0:1], gt_black[0:1], cmap='coolwarm')
+        # # animate_traj2ds_on_rgbs
+        # sw.summ_traj2ds_on_rgb('outputs/single_trajs_on_gt_rgb', trajs_e[0:1], gt_rgb[0:1], cmap='coolwarm', linewidth=2)
+        # sw.summ_traj2ds_on_rgb('outputs/single_trajs_on_gt_black', trajs_e[0:1], gt_black[0:1], cmap='coolwarm', linewidth=2)
 
         rgb_vis = []
-        black_vis = []
-        for trajs_e in preds2:
-            trajs_e = trajs_e.reshape(B_bak, N2_bak, S, 2).permute(0, 2, 1, 3) + pad
-            rgb_vis.append(sw.summ_traj2ds_on_rgb('', trajs_e[0:1], gt_rgb, only_return=True, cmap='coolwarm'))
-            black_vis.append(sw.summ_traj2ds_on_rgb('', trajs_e[0:1], gt_black, only_return=True, cmap='coolwarm'))
-        sw.summ_rgbs('outputs/animated_trajs_on_black', black_vis)
+        for trajs_e_ in preds_anim:
+            trajs_e_ = trajs_e_ + pad
+            rgb_vis.append(sw.summ_traj2ds_on_rgb('', trajs_e_[0:1], torch.mean(utils.improc.preprocess_color(rgbs[0:1]), dim=1), only_return=True, cmap='spring', linewidth=linewidth))
         sw.summ_rgbs('outputs/animated_trajs_on_rgb', rgb_vis)
 
-    return total_loss, metrics
+    return trajs_e-pad
     
 def train():
 
@@ -333,6 +203,15 @@ def train():
     exp_name = 'pu24' # 130k au31
     exp_name = 'pu25' # 155k au53
     exp_name = 'pu26' # 16**2
+    exp_name = 'pu27' # run on orion; tb79
+    exp_name = 'pu28' # stride4
+    exp_name = 'pu29' # actually load please
+    exp_name = 'pu30' # wide vis
+    exp_name = 'pu31' # 32**2
+    exp_name = 'pu32' # linewidth=1
+    exp_name = 'pu33' # just wide vis
+    exp_name = 'pu33' # linewidth=3, N2=8**2
+    exp_name = 'pu34' # re-test; show anim
 
     init_dir = 'checkpoints/04_8_256_1e-4_p1_A_big08_01:08:24'
     init_dir = 'checkpoints/12_8_256_1e-4_p1_A_big14_22:20:05'
@@ -343,18 +222,20 @@ def train():
     init_dir = 'checkpoints/04_8_64_10_2e-4_p1_au48_17:32:27'
     init_dir = 'checkpoints/02_8_128_96_1e-4_p1_au31_12:38:03'
     init_dir = 'checkpoints/04_8_64_10_2e-4_p1_A_au53_19:19:06'
+    init_dir = 'saved_checkpoints/4hv_8_128_I6_4e-4_p1_A_tb79_09:11:39' # new best
 
     ## choose hyps
     B = 1
     S = 8
     N = 256 # we need to load at least 4 i think
-    # N2 = 144
-    N2 = 16**2
+    N2 = 8**2 # number of points to track
+    N2 = 16**2 # number of points to track
+    # N2 = 32**2 # number of points to track
     lr = 1e-4
     grad_acc = 1
     
-    max_iters = 50
-    log_freq = 4
+    max_iters = 40
+    log_freq = 2
     
     ## autogen a name
     model_name = "%02d_%d_%d_%d" % (B, S, N, N2)
@@ -372,12 +253,7 @@ def train():
 
     global_step = 0
 
-    # model = SparseRelativePerceiver().cuda()
-    # model = StGraphRaft().cuda()
-    # model = StSpRaft().cuda()
-    # model = Praft().cuda()
-    # model = Mpraft().cuda()
-    model = Singlepoint().cuda()
+    model = Singlepoint(stride=4).cuda()
     # model = torch.nn.DataParallel(model).cuda()
     parameters = list(model.parameters())
     if init_dir:
@@ -386,12 +262,13 @@ def train():
     requires_grad(parameters, False)
     model.eval()
 
+    
+
     while global_step < max_iters:
         
         read_start_time = time.time()
         
         global_step += 1
-        total_loss = torch.tensor(0.0, requires_grad=True).to(device)
 
         sw_t = utils.improc.Summ_writer(
             writer=writer_t,
@@ -401,31 +278,33 @@ def train():
             scalar_freq=int(log_freq/2),
             just_gif=True)
 
-        rgbs = []
-        for s in range(S):
-            fn = '../puppy/%06d.jpg' % ((global_step-1)*S + 1 + s + 100)
-            print('fn', fn)
-            im = imageio.imread(fn)
-            im = im.astype(np.uint8)
-            # print('im', im.shape)
-            rgbs.append(torch.from_numpy(im).permute(2,0,1))
-            # rgb = np.asarray(Image.open(os.path.join(self.image_location, img_record)).convert('RGB'))
-            # rgb = rgb[np.newaxis, :, :, :]
-        rgbs = torch.stack(rgbs, dim=0).unsqueeze(0) # 1, S, C, H, W
-        
-        read_time = time.time()-read_start_time
-        iter_start_time = time.time()
-            
-        with torch.no_grad():
-            total_loss, metrics = run_model(model, rgbs, N2, sw_t)
+        try:
+            rgbs = []
+            for s in range(S):
+                fn = '../puppy/%06d.jpg' % ((global_step-1)*S + 1 + s + 100)
+                # print('fn', fn)
+                im = imageio.imread(fn)
+                im = im.astype(np.uint8)
+                # print('im', im.shape)
+                rgbs.append(torch.from_numpy(im).permute(2,0,1))
+                # rgb = np.asarray(Image.open(os.path.join(self.image_location, img_record)).convert('RGB'))
+                # rgb = rgb[np.newaxis, :, :, :]
+            rgbs = torch.stack(rgbs, dim=0).unsqueeze(0) # 1, S, C, H, W
 
-        # sw_t.summ_scalar('total_loss', total_loss)
+            read_time = time.time()-read_start_time
+            iter_start_time = time.time()
 
-        iter_time = time.time()-iter_start_time
-        print('%s; step %06d/%d; rtime %.2f; itime %.2f; loss = %.5f' % (
-            model_name, global_step, max_iters, read_time, iter_time,
-            total_loss.item()))
+            with torch.no_grad():
+                trajs_e = run_model(model, rgbs, N2, sw_t)
+
+
+            iter_time = time.time()-iter_start_time
+            print('%s; step %06d/%d; rtime %.2f; itime %.2f' % (
+                model_name, global_step, max_iters, read_time, iter_time))
+        except FileNotFoundError as e:
+            print('error', e)
             
+
     writer_t.close()
             
 
