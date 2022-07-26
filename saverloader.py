5c2f9c55ee846f086f71a1f9e79e2555b542e5c7
aharley <harley.adam@gmail.com>
2022-07-26 00:34:56 -0700

    removed junk from saverloader

--- saverloader.py
+++ saverloader.py
@@ -1,6 +1,5 @@
 import torch
 import os, pathlib
-# import hyperparams as hyp
 import numpy as np
 
 def save(ckpt_dir, optimizer, model, global_step, scheduler=None, model_ema=None, keep_latest=5, model_name='model'):
@@ -43,17 +42,6 @@ def load(ckpt_dir, model, optimizer=None, scheduler=None, model_ema=None, step=0
                 print('ignoring', ignore_load)
 
                 checkpoint = torch.load(path)['model_state_dict']
-                # model.load_state_dict(checkpoint['model_state_dict'], strict=False)
-
-                # model_state = model.state_dict()
-                # for name, param in checkpoint.items():
-                #     if name not in model_state:
-                #         continue
-                #     if isinstance(param, torch.nn.Parameter):
-                #         # backwards compatibility for serialized parameters
-                #         param = param.data
-                #         model_state[name].copy_(param)
-                # model.load_state_dict(model_state)
 
                 model_dict = model.state_dict()
 
@@ -62,57 +50,10 @@ def load(ckpt_dir, model, optimizer=None, scheduler=None, model_ema=None, step=0
                 for ign in ignore_load:
                     pretrained_dict = {k: v for k, v in pretrained_dict.items() if not ign in k}
                     
-                # pretrained_dict = {k: v for k, v in checkpoint.items() if k in model_dict}
                 # 2. overwrite entries in the existing state dict
                 model_dict.update(pretrained_dict)
                 # 3. load the new state dict
                 model.load_state_dict(model_dict, strict=False)
-                                                                                                             
-                
-                # checkpoint = torch.load(path)['model_state_dict']
-                # # model.load_state_dict(dict([(n, p) for n, p in checkpoint['model_state_dict'].items()]), strict=False)
-
-                # # # keys_vin=torch.load('',map_location=device)
-                
-                # # new_state_dict = {k:v if v.size()==current_model[k].size()  else  current_model[k] for k,v in zip(current_model.keys(), checkpoint['model_state_dict'].values())}
-                # # # model.load_state_dict(checkpoint['model_state_dict'], strict=False)
-                # # model.load_state_dict(new_state_dict, strict=False)
-
-                # current_model_dict = model.state_dict()
-
-                # for k,v in zip(current_model_dict.keys(), checkpoint.values()):
-                #     # print('k', k)
-                #     if not (v.size()==current_model_dict[k].size()):
-                #         print('shape mismatch for', k, v.size(), current_model_dict[k].size())
-                #     # print('v', v)
-                #     # v.size()
-                #     # k:v if v.size()==current_model_dict[k].size()  else  current_model_dict[k] 
-                
-                # new_state_dict={k:v if v.size()==current_model_dict[k].size()  else  current_model_dict[k] for k,v in zip(current_model_dict.keys(), checkpoint.values())}
-                # model.load_state_dict(new_state_dict, strict=False)
-
-                # # def get_attr(obj, names):
-                #     if len(names) == 1:
-                #         getattr(obj, names[0])
-                #     else:
-                #         get_attr(getattr(obj, names[0]), names[1:])
-                        
-                # def set_attr(obj, names, val):
-                #     if len(names) == 1:
-                #         setattr(obj, names[0], val)
-                #     else:
-                #         set_attr(getattr(obj, names[0]), names[1:], val)
-                        
-                # for key, dict_param in checkpoint.items():
-                #     submod_names = key.split(".")
-                #     curr_param = get_attr(mod, submod_names)
-                #     new_param = your_processing(curr_param, dict_param)
-                #     # Here you can either replace the existing one
-                #     set_attr(mod, subdmod_names, new_param)
-                #     # Or re-use it (as done in load_checkpoint) but the sizes have to match!
-                #     with torch.no_grad():
-                #         curr_param.copy_(new_param)
-
             else:
                 checkpoint = torch.load(path)
                 model.load_state_dict(checkpoint['model_state_dict'], strict=False)
