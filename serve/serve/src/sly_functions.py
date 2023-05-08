be24bacd80be1484c4c195136f16002909356a0f
Theo <theo.lisin@gmail.com>
2023-05-08 17:50:41 +0300

    added device control

--- serve/serve/src/sly_functions.py
+++ serve/serve/src/sly_functions.py
@@ -6,8 +6,10 @@ from __future__ import unicode_literals
 import cv2
 import supervisely as sly
 from supervisely.geometry.geometry import Geometry
-import sly_globals as g
-from pathlib import Path
+
+# import sly_globals as g
+# from pathlib import Path
+from typing import Tuple
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -17,26 +19,26 @@ from nets.pips import Pips
 
 
 FRAMES_PER_ITER = 8
-H_RESIZED, W_RESIZED = 360, 640
 
 
-def pad_origin(h_origin: int, w_origin: int):
+def pad_origin(h_origin: int, w_origin: int, cur_size: Tuple[int, int]):
     h_pad, w_pad = 0, 0
+    H_RESIZED, W_RESIZED = cur_size
     cur_h, cur_w = H_RESIZED, W_RESIZED
 
     while cur_h < h_origin:
         cur_h += H_RESIZED
     else:
         h_pad = cur_h - h_origin
-    
+
     while cur_w < w_origin:
         cur_w += W_RESIZED
     else:
         w_pad = cur_w - w_origin
-    
+
     top_pad, left_pad = h_pad // 2, w_pad // 2
-    bot_pad, right_pad = h_pad - top_pad, w_pad - left_pad 
-    
+    bot_pad, right_pad = h_pad - top_pad, w_pad - left_pad
+
     return left_pad, right_pad, top_pad, bot_pad
 
 
@@ -46,14 +48,20 @@ def check_bounds(points: np.ndarray, h_max: int, w_max: int):
     return points
 
 
-def run_model(model: Pips, frames: torch.Tensor, orig_points: torch.Tensor):
+def run_model(
+    model: Pips,
+    frames: torch.Tensor,
+    orig_points: torch.Tensor,
+    resized_shape: Tuple[int, int],
+    device: str = "cuda",
+):
     points_number = len(orig_points)
     B, S, C, H_origin, W_origin = frames.shape
-    rgbs_ = frames.reshape(B*S, C, H_origin, W_origin)
-    H_resized, W_resized = H_RESIZED, W_RESIZED
-    pads = pad_origin(H_origin, W_origin)
+    rgbs_ = frames.reshape(B * S, C, H_origin, W_origin)
+    H_resized, W_resized = resized_shape
+    pads = pad_origin(H_origin, W_origin, resized_shape)
     rgbs_ = F.pad(rgbs_, pads, "constant", 0)
-    rgbs_ = F.interpolate(rgbs_, (H_resized, W_resized), mode='bilinear')
+    rgbs_ = F.interpolate(rgbs_, (H_resized, W_resized), mode="bilinear")
     rgbs = rgbs_.reshape(B, S, C, H_resized, W_resized)
 
     Rx, Ry = W_origin / W_resized, H_origin / H_resized
@@ -63,42 +71,50 @@ def run_model(model: Pips, frames: torch.Tensor, orig_points: torch.Tensor):
     points[:, 1] = (points[:, 1] + pads[2]) / Ry
     xy0 = points.reshape(B, points_number, 2).int()
 
-    trajs_e = torch.zeros((B, S, points_number, 2), dtype=torch.float32, device='cuda')
+    trajs_e = torch.zeros((B, S, points_number, 2), dtype=torch.float32, device=device)
 
     for n in range(points_number):
         cur_frame = 0
         done = False
-        traj_e = torch.zeros((B, S, 2), dtype=torch.float32, device='cuda')
-        traj_e[:,0] = xy0[:,n] # B, 1, 2  # set first position 
+        traj_e = torch.zeros((B, S, 2), dtype=torch.float32, device=device)
+        traj_e[:, 0] = xy0[:, n]  # B, 1, 2  # set first position
         feat_init = None
         while not done:
             end_frame = cur_frame + FRAMES_PER_ITER
 
-            rgb_seq: torch.Tensor = rgbs[:,cur_frame:end_frame]
-            rgb_seq = rgb_seq.cuda().float() 
+            rgb_seq: torch.Tensor = rgbs[:, cur_frame:end_frame]
+            rgb_seq = rgb_seq.to(torch.device(device)).float()
             S_local = rgb_seq.shape[1]  # may become less then FRAMES_PER_ITER
 
             # add new frames if S_local != FRAMES_PER_ITER
-            rgb_seq = torch.cat([rgb_seq, rgb_seq[:,-1].unsqueeze(1).repeat(1,8-S_local,1,1,1)], dim=1) 
+            rgb_seq = torch.cat(
+                [rgb_seq, rgb_seq[:, -1].unsqueeze(1).repeat(1, 8 - S_local, 1, 1, 1)], dim=1
+            )
 
-            outs = model(traj_e[:,cur_frame].reshape(1, -1, 2), rgb_seq, iters=6, feat_init=feat_init, return_feat=True)
+            outs = model(
+                traj_e[:, cur_frame].reshape(1, -1, 2),
+                rgb_seq,
+                iters=6,
+                feat_init=feat_init,
+                return_feat=True,
+            )
             rgb_seq.cpu()
             torch.cuda.empty_cache()
             preds = outs[0]
-            vis = outs[2] # B, S, 1
+            vis = outs[2]  # B, S, 1
             feat_init = outs[3]
-            
-            vis = torch.sigmoid(vis) # visibility confidence
+
+            vis = torch.sigmoid(vis)  # visibility confidence
             xys = preds[-1].reshape(1, FRAMES_PER_ITER, 2)
-            traj_e[:,cur_frame:end_frame] = xys[:,:S_local]
+            traj_e[:, cur_frame:end_frame] = xys[:, :S_local]
 
             found_skip = False
             thr = 0.9
-            si_last = FRAMES_PER_ITER - 1 # last frame we are willing to take
-            si_earliest = 1 # earliest frame we are willing to take
+            si_last = FRAMES_PER_ITER - 1  # last frame we are willing to take
+            si_earliest = 1  # earliest frame we are willing to take
             si = si_last
             while not found_skip:
-                if vis[0,si] > thr:
+                if vis[0, si] > thr:
                     found_skip = True
                 else:
                     si -= 1
@@ -112,12 +128,17 @@ def run_model(model: Pips, frames: torch.Tensor, orig_points: torch.Tensor):
 
             if cur_frame >= S:
                 done = True
-        trajs_e[:,:,n] = traj_e
+        trajs_e[:, :, n] = traj_e
 
-    trajs_e = trajs_e.squeeze(0) # .permute(1, 0, 2)  # sequence len, points_num, 2
+    trajs_e = trajs_e.squeeze(0)  # .permute(1, 0, 2)  # sequence len, points_num, 2
     trajs_e[:, :, 0] = trajs_e[:, :, 0] * Rx - pads[0]
     trajs_e[:, :, 1] = trajs_e[:, :, 1] * Ry - pads[2]
-    return trajs_e.detach().cpu().numpy()
+    preds = check_bounds(
+        trajs_e.detach().cpu().numpy().squeeze(),
+        h_max=H_origin,
+        w_max=W_origin,
+    )
+    return preds
 
 
 def geometry_to_np(figure: Geometry):
