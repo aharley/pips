41bebbe2ac16a0b673a3552c081333e759f2dbac
aharley <harley.adam@gmail.com>
2023-08-06 13:55:02 -0700

    added debuged positional encodings

--- utils/misc.py
+++ utils/misc.py
@@ -1,6 +1,46 @@
 import torch
 import numpy as np
 
+def posemb_sincos_2d_xy(x, y, temperature=10000, dtype=torch.float32, dim=64):
+    device = x.device
+    dtype = x.dtype
+    B, S = x.shape
+    # _, h, w, dim, device, dtype = *patches.shape, patches.device, patches.dtype
+
+    # y, x = torch.meshgrid(torch.arange(h, device = device), torch.arange(w, device = device), indexing = 'ij')
+    assert (dim % 4) == 0, 'feature dimension must be multiple of 4 for sincos emb'
+    omega = torch.arange(dim // 4, device = device) / (dim // 4 - 1)
+    omega = 1. / (temperature ** omega)
+
+    y = y.flatten()[:, None] * omega[None, :]
+    x = x.flatten()[:, None] * omega[None, :] 
+    pe = torch.cat((x.sin(), x.cos(), y.sin(), y.cos()), dim=1) 
+    pe = pe.reshape(B,S,dim)
+    return pe.type(dtype)
+
+def posemb_sincos_3d(xyz, temperature=10000, dtype=torch.float32, dim=64, cat_coords=False):
+    B,N,D = xyz.shape
+    assert(D==3)
+    device = xyz.device
+    dtype = xyz.dtype
+
+    x, y, z = xyz[:,:,0], xyz[:,:,1], xyz[:,:,2]
+
+    assert (dim % 2) == 0, 'feature dimension must be multiple of 2 for sincos emb'
+    omega = torch.arange(dim // 2, device = device) / (dim // 2 - 1)
+    omega = 1. / (temperature ** omega)
+
+    z = z.flatten()[:, None] * omega[None, :]
+    y = y.flatten()[:, None] * omega[None, :]
+    x = x.flatten()[:, None] * omega[None, :] 
+    pe = torch.cat((x.sin(), x.cos(), y.sin(), y.cos(), z.sin(), z.cos()), dim=1) 
+    pe = pe.reshape(B,N,dim*3)
+
+    if cat_coords:
+        pe = torch.cat([pe, xyz], dim=2) # B, N, dim*3+3
+    
+    return pe.type(dtype)
+
 def get_3d_embedding(xyz, C, cat_coords=True):
     B, N, D = xyz.shape
     assert(D==3)
